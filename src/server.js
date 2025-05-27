1c298fb853d6193c3d5c43d9182c8cc09eb9c490
unknown <mhmad.maksousa@gmail.com>
2025-05-27 20:27:59 +0300

    deploy

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(logger('dev'));
 //allow the client to access the server
 app.options('*', cors());
 //to synchronizing all models at once
-sequelize.sync({ alter: false }).then((_) => {
+sequelize.sync({ alter: true }).then((_) => {
     console.log("connected to db successfully");
     app.use(express.urlencoded({ extended: false }));
     // cors policies
